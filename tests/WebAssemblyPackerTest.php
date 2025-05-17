adef5cbd4dd7765d901f1493fe16b4e49bc6c477
Bohuslav Šimek <bohuslav@simek.si>
2025-05-17 13:29:14 +0200

    first blood

--- tests/WebAssemblyPackerTest.php
+++ tests/WebAssemblyPackerTest.php
@@ -30,8 +30,6 @@ class WebAssemblyPackerTest extends TestCase
 
     public function testPackWithoutLz4()
     {
-        echo "Starting testPackWithoutLz4\n";
-        
         // Create build directory if it doesn't exist
         if (!is_dir($this->buildDir)) {
             mkdir($this->buildDir, 0777, true);
@@ -52,7 +50,8 @@ class WebAssemblyPackerTest extends TestCase
         // Create argv array similar to command line arguments
         $argv = [
             'filepackager.php',
-            $this->buildDir . '/php-web.data',
+            //$this->buildDir . '/php-web.data',
+            'tests/fixtures/build-php/php-web.data',
             '--preload', $this->fixturesDir . '/test_dir',
             '--js-output=' . $this->buildDir . '/php-web.data.js',
             '--use-preload-cache',
@@ -89,11 +88,12 @@ class WebAssemblyPackerTest extends TestCase
         // Add test directory to initialDataFiles
         $testDir = $this->fixturesDir . '/test_dir';
         $options->initialDataFiles[] = new DataFile($testDir, $testDir, 'preload', false);
-
+//'build-php/php-web.data',
         // Create argv array similar to command line arguments
         $argv = [
             'filepackager.php',
-            $this->buildDir . '/php-web.data',
+            //$this->buildDir . '/php-web.data',
+            'tests/fixtures/build-php/php-web.data',
             '--preload', $this->fixturesDir . '/test_dir',
             '--js-output=' . $this->buildDir . '/php-web.data.js',
             '--lz4',
@@ -120,7 +120,7 @@ class WebAssemblyPackerTest extends TestCase
         // Compare with expected files
         $this->assertEquals(
             file_get_contents($this->buildDir . '/php-web.data'),
-            file_get_contents($this->fixturesDir . '/testing/no-compression.data'),
+            file_get_contents($this->fixturesDir . '/testing2/no-compression.data'),
             'Data files are not equal'
         );
 
@@ -131,15 +131,13 @@ class WebAssemblyPackerTest extends TestCase
 
         $this->assertEquals(
             file_get_contents($this->buildDir . '/php-web.data.js'),
-            file_get_contents($this->fixturesDir . '/testing/php-web.data.js'),
+            file_get_contents($this->fixturesDir . '/testing2/php-web.data.js'),
             'JS files are not equal'
         );
     }
 
     public function testCompareFilesLz4()
     {
-        echo "Starting testCompareFilesLz4\n";
-        
         // Run both packers
         $this->testPackWithoutLz4();
         $this->testPackWithLz4();
@@ -147,7 +145,7 @@ class WebAssemblyPackerTest extends TestCase
         // Compare with expected files
         $this->assertEquals(
             file_get_contents($this->buildDir . '/php-web.data'),
-            file_get_contents($this->fixturesDir . '/testing/compression.data'),
+            file_get_contents($this->fixturesDir . '/testing2/compression.data'),
             'Data files are not equal'
         );
 
@@ -157,10 +155,8 @@ class WebAssemblyPackerTest extends TestCase
 
         $this->assertEquals(
             file_get_contents($this->buildDir . '/php-web.data.js'),
-            file_get_contents($this->fixturesDir . '/testing/compression-web.data.js'),
+            file_get_contents($this->fixturesDir . '/testing2/compression-web.data.js'),
             'JS files are not equal'
         );
-        
-        echo "Finished testCompareFilesLz4\n";
     }
 }
